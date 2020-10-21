bc88d50440c2444196775f5b04527d71ca552f86
onlinepdfgallery <67324470+onlinepdfgallery@users.noreply.github.com>
2020-10-21 18:37:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// alert('helooo');
+// console.log(1);
+// console.log(3);
+// let age=25;
+// let year=20;
+// console.log(age,year);
+const title= 'aaaabbbxxxdd';
+const author = 'mario';
+const likes=30;
+// let result= 'the blog called ' +  title + ' by '+  author + ' likes ' +likes;
+// let results = " the blog called ${title} by ${author} has ${likes} likes ";
+let html =`
+         <h2> ${title} </h2>
+         <p> ${author} </p
+          <span> this blog has ${likes}< /span >`;
+console.log(html);
